9b331da55c74a7d81ab677d804e6d69b7ff60928
Eric Albert Alonso <dulothings@gmail.com>
2025-11-03 19:41:34 +0100

    Tests correctos

--- src/main/java/com/pim/jrgs2526/MyDate.java
+++ src/main/java/com/pim/jrgs2526/MyDate.java
@@ -1,5 +1,8 @@
 package com.pim.jrgs2526;
 
+import java.time.Year;
+import java.time.YearMonth;
+
 public class MyDate {
 
     public static final String ERR_INVALID_YEAR = "Year value not valid";
@@ -35,17 +38,55 @@ public class MyDate {
         }
     }
 
-
+    private int day;
+    private Months month;
+    private int year;
 
     public MyDate(){
 
     }
-    public MyDate(int day, Months month, int year){
+    public MyDate(int day, Months month, int year) {
+        int mes = month.monthNumber;
+        int diasMes = YearMonth.of(this.year,mes).lengthOfMonth();
 
+        boolean esBisiesto = (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
+        if (month == Months.FEBRUARY && !esBisiesto && day > 28) {
+            throw new IllegalArgumentException(ERR_INVALID_DATE);
+        }
+
+        if (day < 1 || day > diasMes) {
+            throw new IllegalArgumentException(ERR_INVALID_DATE);
+        }
+
+        this.day = day;
+        this.month = month;
+        this.year = year;
     }
-    public void setDay(int day){}
+    public void setDay(int day){
+        int mes = month.monthNumber;
+        int diasMes = YearMonth.of(this.year,mes).lengthOfMonth();
+        if(day > diasMes || day <= 0){
+            throw new IllegalArgumentException(ERR_INVALID_DAY);
+        }
+        this.day = day;
+    }
 
-    public void setMonth(Months month){}
+    public void setMonth(Months month){
+        int mes = month.monthNumber;
+        int diasMes = YearMonth.of(this.year,mes).lengthOfMonth();
+        if(this.day > diasMes){
+            throw new IllegalArgumentException(ERR_INVALID_MONTH);
+        }
+        this.month = month;
+    }
 
-    public void setYear(int year){}
+    public void setYear(int year){
+        int mes = month.monthNumber;
+        int diasMes = YearMonth.of(year,mes).lengthOfMonth();
+
+        if (day < 1 || day > diasMes) {
+            throw new IllegalArgumentException(ERR_INVALID_YEAR);
+        }
+        this.year = year;
+    }
 }
